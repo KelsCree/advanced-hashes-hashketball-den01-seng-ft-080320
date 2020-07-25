f411ab79cc84c978dec4169c1b7c7c560e6c95e8
Kelsey Creehan <kelseycreehan@gmail.com>
2020-07-25 19:15:29 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -194,11 +194,15 @@ def player_stats(player_search)
 end
 
 def big_shoe_rebounds
+  big_shoe = 0
+  rebounds = 0
   game_hash.each do |team, team_info|
-    team_info.each do |key, value|
-      if key == :players
-        value.each do |key2, value2|
-          if key2 == :shoe
-            
-          
+    team_info.each do |player|
+      if player[:shoe] > big_shoe
+        big_shoe = player[:shoe]
+        rebounds = player[:rebounds]
+        end
+      end
+    end
+    rebounds
 end
